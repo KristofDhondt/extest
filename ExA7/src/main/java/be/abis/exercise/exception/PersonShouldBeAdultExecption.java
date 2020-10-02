bea9972532089fd619b5ad7d516926776473d8d4
KristofDhondt <kristof.dhondt@bnpparibasfortis.com>
2020-10-02 16:09:53 +0200

    add A7

---
+++ ExA7/src/main/java/be/abis/exercise/exception/PersonShouldBeAdultExecption.java
@@ -0,0 +1,7 @@
+package be.abis.exercise.exception;
+
+public class PersonShouldBeAdultExecption extends Exception {
+    public PersonShouldBeAdultExecption(String message) {
+        super(message);
+    }
+}
