7f8f943445b54bb2d5b7ed158dc7099ea7425001
KristofDhondt <kristof.dhondt@bnpparibasfortis.com>
2020-10-02 11:23:17 +0200

    exercise 2

--- EXA1/src/test/java/be/abis/exercise/ut/TestPerson.java
+++ EXA1/src/test/java/be/abis/exercise/ut/TestPerson.java
@@ -1,11 +1,17 @@
 package be.abis.exercise.ut;
 
+import be.abis.exercise.model.Person;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
 
 public class TestPerson {
 
     @Test
     public void personShouldBe42() {
+        Person persoon = new Person(1, "Kristof", "Dhondt", LocalDate.of(1977, 11, 8));
+        assertEquals(persoon.calculateAge(), 42);
+    }
 
     }
-}
