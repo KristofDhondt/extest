f7a7a00bfd81494e78b5184521a5d97de9f126e3
KristofDhondt <kristof.dhondt@bnpparibasfortis.com>
2020-10-05 11:49:49 +0200

    add EXB3

---
+++ ExB4/src/test/java/be/abis/exercise/ut/TestMyTestSuite.java
@@ -0,0 +1,10 @@
+package be.abis.exercise.ut;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {TestAdress.class , TestPerson.class } )
+    public class TestMyTestSuite {
+}
